2ab0ce505e67d639b64e8fe3362ca6f251d0b107
bliss81 <40465324+bliss81@users.noreply.github.com>
2021-01-06 10:33:30 +0200

    Update dockerinst.sh

--- dockerinst.sh
+++ dockerinst.sh
@@ -1,3 +1,4 @@
+sudo apt install curl
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
