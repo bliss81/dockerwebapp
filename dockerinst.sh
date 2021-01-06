bfad8aaf01a741c7910bc6d30ef83acc2beae2e6
bliss81 <40465324+bliss81@users.noreply.github.com>
2021-01-06 11:14:04 +0200

    Update dockerinst.sh

--- dockerinst.sh
+++ dockerinst.sh
@@ -1,8 +1,7 @@
-sudo apt install curl
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo groupadd docker
+sudo apt update
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
 sudo usermod -aG docker ${USER}
+
